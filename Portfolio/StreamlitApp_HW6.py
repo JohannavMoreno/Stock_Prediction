e7d08df8bb88b4c3270d2618cdf8b26a39b916db
JohannavMoreno <johanna.moreno@tcu.edu>
2026-04-23 07:20:48 -0500

    Update StreamlitApp_HW6.py

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -63,8 +63,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_sentiment.shap',
         "pipeline": 'finalized_sentiment_model.tar.gz',
-        "keys": ['ADBE','AMZN','WMT','PredictedSentiment'],
-       "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ['ADBE','AMZN','WMT','PredictedSentiment']]
+        "keys": ['ADBE', 'MSFT', 'JPM', 'sentiment_textblob'],
+       "inputs": [{"name": k, "type": "number", "min": -10.0, "max": 10.0, "default": 0.0, "step": 0.01} for k in ['ADBE', 'MSFT', 'JPM', 'sentiment_textblob']]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -113,7 +113,7 @@ def call_model_api(input_df):
         # For classification
         raw_pred = predictor.predict(input_df)
         pred_val = pd.DataFrame(raw_pred).values[-1][0]
-        mapping = {0: "SELL", 1: "HOLD", 2: "BUY"}
+        mapping = {-1: "SELL", 0: "HOLD", 1: "BUY"}
         return mapping.get(pred_val), 200
     except Exception as e:
         return f"Error: {str(e)}", 500
